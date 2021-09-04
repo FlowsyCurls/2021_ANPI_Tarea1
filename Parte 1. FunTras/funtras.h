b02a56359a22ebfecccf6da8dcdf06e7c2d86374
sebas-mora28 <semasmora@gmail.com>
2021-09-04 09:10:03 -0600

    Implementacion utilizando boost.multiprecision

--- Parte 1. FunTras/funtras.h
+++ Parte 1. FunTras/funtras.h
@@ -5,10 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
-double tol = 0.00000001;
+
+/**
+ * Parte 1: Librería FunTras(C++)
+ * 
+ * Se debe instalar la biblioteca boot
+ * Paso 1: Ejecutar los siguientes comandos ->  sudo apt-get updade
+ *                                              sudo apt-get install libboost-all-dev
+ * 
+ */
+
+
+
+using namespace boost::multiprecision;
+cpp_dec_float_50 tol = 0.00000001;
 int iterMax = 5000;
-
 
 /**
 * Implementacion de la funcion factorial
@@ -20,11 +33,13 @@ int iterMax = 5000;
 *          - res : resultado final. 
 *
 */
-double factorial(int x)
+
+
+cpp_dec_float_50 factorial(int x)
 {
     
     int temp = 1; 
-    double res = 1.;
+    cpp_dec_float_50 res = 1.;
 
     if (x < 0) //En caso de que el argumento sea menor que cero, no existe un factorial 
     {
@@ -57,23 +72,21 @@ double factorial(int x)
 
 // --------------------------- funcion x^-1 -----------------------------------------
 
-double div_ta(double a)
+cpp_dec_float_50 div_ta(cpp_dec_float_50 a)
 {
-    double eps = 2.2204E-16;
-    double x0;
+    cpp_dec_float_50 eps = 2.2204E-16;
+    cpp_dec_float_50 x0;
 
-    double fac0 = 1;
-    double fac20 = factorial(20);
-    double fac40 = factorial(40);
-    double fac60 = factorial(60);
-    double fac80 = factorial(80);
-    double fac100 = factorial(100);
+    cpp_dec_float_50 fac20 = factorial(20);
+    cpp_dec_float_50 fac40 = factorial(40);
+    cpp_dec_float_50 fac60 = factorial(60);
+    cpp_dec_float_50 fac80 = factorial(80);
+    cpp_dec_float_50 fac100 = factorial(100);
 
 
-    if (fac0 < a <= fac20)
+    if (0 < a <= fac20)
     {
         x0 = pow(eps, 2);
-        std::cout << x0 << '\n';
     }
 
     if (fac20 < a <= fac40)
@@ -97,7 +110,7 @@ double div_ta(double a)
     }
 
 
-    double x1, error = tol + 1;
+    cpp_dec_float_50 x1, error = tol + 1;
     int k = 0; 
 
  
@@ -130,21 +143,26 @@ double div_ta(double a)
 *          - sk1 : resultado final. 
 */
 
-double exp_t(double x)
+cpp_dec_float_50 exp_t(cpp_dec_float_50 x)
 {
 
 
     int n = 2;
-    long double sk = 1; //Se define el valor inicial de sk
-    long double sk1 = sk + x; //Se define el valor inicial de sk+1
+    cpp_dec_float_50 sk = 1; //Se define el valor inicial de sk
+    cpp_dec_float_50 sk1 = sk + x; //Se define el valor inicial de sk+1
     int k = 1;
-    double error = tol + 1;
+    cpp_dec_float_50 error = tol + 1;
     
     while (error >= tol && k <= iterMax )
     {
         sk = sk1;
-        sk1 += pow(x, n) / factorial(n); //Se calcula el nuevo valor de sk1
-        error = fabsl(sk1 - sk);
+
+        cpp_dec_float_50 fact = factorial(n);
+        cpp_dec_float_50 d = div_ta(fact); 
+
+        sk1 += pow(x, n) * d; //Se calcula el nuevo valor de sk1
+
+        error = fabs(sk1 - sk);
         n++;
     }
 
@@ -156,9 +174,8 @@ double exp_t(double x)
 //------------------------------- funcion sin(x) ------------------------------------
 
 
-
 /**
-* Aproximacion de la funcion sen
+* Aproximacion de la funcion sen(x)
 * 
 * Paramatros de entrada 
 *          - x : argumento de la funcion cos
@@ -168,24 +185,28 @@ double exp_t(double x)
 */
 
 
-double sin_t(double x)
+cpp_dec_float_50 sin_t(cpp_dec_float_50 x)
 {
 
 
-    double n  = 0;
-    double sk = 0; //Se define el valor inicial de sk
-    double sk1 =0; //Se define el valor inicial de sk+1
+    int n  = 0;
+    cpp_dec_float_50 sk = 0; //Se define el valor inicial de sk
+    cpp_dec_float_50 sk1 =0; //Se define el valor inicial de sk+1
     int k = 1;
-    double error = tol + 1;
+    cpp_dec_float_50 error = tol + 1;
 
 
    
     while (error >= tol && k <= iterMax)
     {
-        sk = sk1;
+        sk = sk1; //Se asgina el nuevo valor de sk 
         int arg = 2*n + 1;
-        sk1 += pow(-1,n)*(pow(x, arg) / factorial(arg));//Se calcula el nuevo valor de sk1
-        error = fabsl(sk1 - sk);
+
+        cpp_dec_float_50 fact = factorial(arg);
+        cpp_dec_float_50 d = div_ta(fact);
+
+        sk1 += pow(-1,n)*(pow(x, arg) * d);//Se calcula el nuevo valor de sk1
+        error = fabs(sk1 - sk); //Se calcula el error
         k++;
         n++;
     }
@@ -200,7 +221,7 @@ double sin_t(double x)
 
 
 /**
-* Aproximacion de la funcion cos
+* Aproximacion de la funcion cos(x)
 * 
 * Paramatros de entrada 
 *          - x : argumento de la funcion cos
@@ -211,23 +232,27 @@ double sin_t(double x)
 
 
 
-double cos_t(double x)
+cpp_dec_float_50 cos_t(cpp_dec_float_50 x)
 {
 
 
     int n = 0;
-    double sk = 0; //Se define el valor inicial de sk
-    double sk1 = 0; //Se define el valor inicial de sk+1
+    cpp_dec_float_50 sk = 0; //Se define el valor inicial de sk
+    cpp_dec_float_50 sk1 = 0; //Se define el valor inicial de sk+1
     int k = 1;
-    double error = tol + 1;
+    cpp_dec_float_50 error = tol + 1;
 
 
     while (error >= tol && k <= iterMax)
     {
-        sk = sk1;
+        sk = sk1; //Se asigna el nuevo valor de sk
         int arg = 2*n;
-        sk1 += pow(-1,n)*(pow(x, arg) / factorial(arg));
-        error = fabsl(sk1 - sk);
+
+        cpp_dec_float_50 fact = factorial(arg);
+        cpp_dec_float_50 d = div_ta(fact);
+
+        sk1 += pow(-1,n)*(pow(x, arg) * d); //Se calcula el nuevo valor de xk1
+        error = fabs(sk1 - sk);
         k++;
         n++;
     }
@@ -252,10 +277,13 @@ double cos_t(double x)
 * 
 */
 
-double tan_t(double x)
+cpp_dec_float_50 tan_t(cpp_dec_float_50 x)
 {
 
-    return sin_t(x)/cos_t(x);
+    cpp_dec_float_50 cos = cos_t(x); //Se calcula el valor de cos(x) 
+    cpp_dec_float_50 d = div_ta(cos); //Se calcula su inverso 
+
+    return sin_t(x) * d; // Multiplica por el sen(x) obteniendo el valor de la tangente. 
 
 }
 
@@ -265,7 +293,7 @@ double tan_t(double x)
 //------------------------------- funcion ln(x) ------------------------------------
 
 /**
-* aproximacion de la funcion logaritmo natural. Mediante la utilizacion de una serie.
+* Aproximacion de la funcion logaritmo natural.
 * 
 * Paramatros de entrada 
 *          - a : argumento de la funcion logaritmo, debe estrictamente mayor que cero 
@@ -277,7 +305,7 @@ double tan_t(double x)
 */
 
 
-double ln_t(double x)
+cpp_dec_float_50 ln_t(cpp_dec_float_50 x)
 {
 
 
@@ -289,18 +317,22 @@ double ln_t(double x)
     }
 
     int n = 0;
-    double sk = 0; //Se define el valor inicial de sk
-    double sk1 = 0; //Se define el valor inicial de sk+1
+    cpp_dec_float_50 sk = 0; //Se define el valor inicial de sk
+    cpp_dec_float_50 sk1 = 0; //Se define el valor inicial de sk+1
     int k = 1;
-    double error = tol + 1;
+    cpp_dec_float_50 error = tol + 1;
 
 
     
     while (error >= tol && k <= iterMax)//Parametros de parada 
     {
         sk = sk1;
-        sk1 += (2.*(x-1)/(x+1))*(1./(2*n+1)* pow(((x-1)/(x+1)), 2*n)); //aproximacion del nuevo sk1
-        error = fabsl(sk1 - sk);
+
+        cpp_dec_float_50 d1 = div_ta(x + 1);
+        cpp_dec_float_50 d2 = div_ta(2.*n + 1);
+
+        sk1 += (2.*(x-1) * d1)*((d2)* pow(((x-1) * d1), 2*n)); //Aproximacion del nuevo sk1
+        error = fabs(sk1 - sk); //Calculo del error
         n++; //aumenta el exponente
 
     }
@@ -329,7 +361,7 @@ double ln_t(double x)
 */
 
 
-double log_t(double x, double a)
+cpp_dec_float_50 log_t(cpp_dec_float_50 x, cpp_dec_float_50 a)
 {
 
     if (x <= 0) //Se verifica que x sea estrictamente mayor que cero para que cumpla el dominio de la funcion 
@@ -345,7 +377,10 @@ double log_t(double x, double a)
 
     }
 
-    return ln_t(x)/ln_t(a); //Se reescribe la expresion de logaritmo en terminos de logaritmo natual y se 
+    cpp_dec_float_50 ln = ln_t(a);
+    cpp_dec_float_50 d = div_ta(ln);
+
+    return ln_t(x) * d; //Se reescribe la expresion de logaritmo en terminos de logaritmo natual y se 
                             //utiliza la funcion anteriormente implementada 
 }
 
@@ -366,12 +401,18 @@ double log_t(double x, double a)
 * 
 */
 
-double power_t(double x, double a)
+cpp_dec_float_50 power_t(cpp_dec_float_50 x, cpp_dec_float_50 a)
 {
 
+    if (a <= 0){
+        std::cout << "Base de la exponencial incorrecta, debe ser mayor que 0";
+        return NULL; 
+        
+    }
 
-    double m = ln_t(a);
-    double exp = exp_t(m*x);
+
+    cpp_dec_float_50 m = ln_t(a);
+    cpp_dec_float_50 exp = exp_t(m*x);
     return exp;
  
 }
@@ -392,22 +433,28 @@ double power_t(double x, double a)
 * 
 */
 
-double asin_t(double x)
+cpp_dec_float_50 asin_t(cpp_dec_float_50 x)
 {
 
     int k = 0;
-    double error = tol + 1;
-    double Sk = 0, Sk0 = 0;;
+    cpp_dec_float_50 error = tol + 1;
+    cpp_dec_float_50 Sk = 0, Sk0 = 0;;
     int n = 0;
 
     while (error > tol && k < iterMax)
     {
-        double fac = factorial(n);
-        Sk += (factorial(2*n)/(pow(4, n)*pow(fac, 2)*(2*n +1)))*pow(x, 2*n + 1); //Calculo del sk de la serie
-        n++;
+
+
+        cpp_dec_float_50 fac = factorial(n);
+        cpp_dec_float_50 k = pow(4, n)*pow(fac, 2)*(2*n +1);
+        cpp_dec_float_50 d = div_ta(k);
+
+        Sk += (factorial(2*n) * d)*pow(x, 2*n + 1); //Calculo del sk de la serie
+        
+        n++; 
         k++;
         error = fabs(Sk - Sk0); //Se calcula el error
-        Sk0 = Sk; 
+        Sk0 = Sk; //Se calcula el nuevo valor de sk0 
 
     }
     return Sk;
@@ -432,7 +479,7 @@ double asin_t(double x)
 *
 */
 
-double f(double x, int p, double a)
+cpp_dec_float_50 f(cpp_dec_float_50 x, int p, cpp_dec_float_50 a)
 {
 
  
@@ -453,7 +500,7 @@ double f(double x, int p, double a)
 *
 */
 
-double derivate_f(double x, int p)
+cpp_dec_float_50 derivate_f(cpp_dec_float_50 x, int p)
 {
 
     return p* pow(x, p - 1);
@@ -466,13 +513,15 @@ double derivate_f(double x, int p)
 * aproximacion de la funcion raiz con un indice dada por el usuario. Se utiliza el metodo 
 * metodo Newton-Rapshon para aproximar el cero de la funcion g(x) = x^p - a
 * 
-* Paramatros de entrada 
+* Parametros de entrada 
 *          - x : argumento de la funcion raiz, debe estrictamente mayor que cero 
 *          - a : indice de la funcion raiz
 * 
+*Parametro de salida
+*          - xk : aproximación de la función 
 */
 
-double root_t(double x, int a)
+cpp_dec_float_50 root_t(cpp_dec_float_50 x, int a)
 {
 
    
@@ -488,18 +537,21 @@ double root_t(double x, int a)
     }
 
 
-    double error = tol + 1;
+    cpp_dec_float_50 error = tol + 1;
     int k = 1;
-    double xk = x/2.; //Se asigna el valor inicial 
+    cpp_dec_float_50 xk = x/2.; //Se asigna el valor inicial 
 
     while(error > tol && k < iterMax) //Se verifica el punto parada
     {
 
 
-        double n = f(xk, a, x); //Se calcula el numerador de la fraccion que forma para del calculo del nuevo xk segun el metodo Newton-Rapshon, se evalua la funcion en xk
-        double d = derivate_f(xk, a); //Se calcula el denomiador de la fraccion que forma para del calculo del nuevo xk segun el metodo Newton-Rapshon, se evalua la derivada funcion en xk
-        double x0 = xk; //Se asigna el nuevo valor para el xk anterior
-        xk = xk - n/d; //Se calcula el nuevo valor de xk
+        cpp_dec_float_50 n = f(xk, a, x); //Se calcula el numerador de la fraccion que forma para del calculo del nuevo xk segun el metodo Newton-Rapshon, se evalua la funcion en xk
+        cpp_dec_float_50 diff = derivate_f(xk, a); //Se se evalua la derivada funcion en xk
+        cpp_dec_float_50 x0 = xk; //Se asigna el nuevo valor para el xk anterior
+        cpp_dec_float_50 d = div_ta(diff);
+        xk = xk - n * d; //Se calcula el nuevo valor de xk
+
+
         error = abs((xk - x0) / xk); //Se calcula el error 
         k++;
 
@@ -525,7 +577,7 @@ double root_t(double x, int a)
 * 
 */
 
-double sqrt_t(int x)
+cpp_dec_float_50 sqrt_t(int x)
 {
 
     return root_t(x, 2);
@@ -538,7 +590,7 @@ double sqrt_t(int x)
 
 //------------------------------- aproximacion de pi ------------------------------------
 
-double pi()
+cpp_dec_float_50 pi()
 {
 
     /**
@@ -548,16 +600,19 @@ double pi()
     */
 
     int n = 0;
-    double sk = 0; //Se define el valor inicial de sk
-    double sk1 = 0; //Se define el valor inicial de sk+1
+    cpp_dec_float_50 sk = 0; //Se define el valor inicial de sk
+    cpp_dec_float_50 sk1 = 0; //Se define el valor inicial de sk+1
     int k = 1;
-    double error = tol + 1;
+    cpp_dec_float_50 error = tol + 1;
     
     while (error >= tol && k <= iterMax)
     {
         sk = sk1;
-        sk1 += pow(-1, n)/(2.*n + 1); //Se calcula el nuevo valor de sk1
-        error = fabsl(sk1 - sk);
+
+        cpp_dec_float_50 d =  div_ta(2.*n + 1);
+
+        sk1 += pow(-1, n) * d; //Se calcula el nuevo valor de sk1
+        error = fabs(sk1 - sk); //Se calcula el valor del error
         n++;
         k++;
 
--- Parte 1. FunTras/test_funtras.cpp
+++ Parte 1. FunTras/test_funtras.cpp
@@ -1,122 +1,69 @@
 #include "funtras.h"
 #include <iostream>
-#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
 
 
 
 
-void print_factorials()
-{
-   using boost::multiprecision::cpp_int;
-   //
-   // Print all the factorials that will fit inside a 128-bit integer.
-   //
-   // Begin by building a big table of factorials, once we know just how
-   // large the largest is, we'll be able to "pretty format" the results.
-   //
-   // Calculate the largest number that will fit inside 128 bits, we could
-   // also have used numeric_limits<int128_t>::max() for this value:
-   cpp_int limit = (cpp_int(1) << 128) - 1;
-   //
-   // Our table of values:
-   std::vector<cpp_int> results;
-   //
-   // Initial values:
-   unsigned i = 1;
-   cpp_int factorial = 1;
-   //
-   // Cycle through the factorials till we reach the limit:
-   while(factorial < 100)
-   {
-      results.push_back(factorial);
-      ++i;
-      factorial *= i;
-   }
-   //
-   // Lets see how many digits the largest factorial was:
-   unsigned digits = results.back().str().size();
-   //
-   // Now print them out, using right justification, while we're at it
-   // we'll indicate the limit of each integer type, so begin by defining
-   // the limits for 16, 32, 64 etc bit integers:
-   cpp_int limits[] = {
-      (cpp_int(1) << 16) - 1,
-      (cpp_int(1) << 32) - 1,
-      (cpp_int(1) << 64) - 1,
-      (cpp_int(1) << 128) - 1,
-   };
-   std::string bit_counts[] = { "16", "32", "64", "128" };
-   unsigned current_limit = 0;
-   for(unsigned j = 0; j < results.size(); ++j)
-   {
-      if(limits[current_limit] < results[j])
-      {
-         std::string message = "Limit of " + bit_counts[current_limit] + " bit integers";
-         std::cout << std::setfill('.') << std::setw(digits+1) << std::right << message << std::setfill(' ') << std::endl;
-         ++current_limit;
-      }
-      std::cout << std::setw(digits + 1) << std::right << results[j] << std::endl;
-   }
-}
 
 int main()
 {
 
 
-    print_factorials();
-    double res = div_ta(170);
-    double res1 = exp_t(-1);
-    double res2 = sin_t(100);
-    double res3 = cos_t(-10);
-    double res4 = tan_t(0.34);
-    double res5 = ln_t(98);
-    double res6 = log_t(75,3);
-    double res7 = power_t(0.8,3);
-    double res8 = pi();
-    double res9 = sqrt_t(2);
-    double res10 = root_t(6,3);
-    double res11 = asin_t(0.23);
 
-    std::cout << "div_t " << res << '\n';
-    std::cout << "exp " << res1 << '\n';
-    std::cout << "sin " <<res2 << '\n';
-    std::cout << "cos " <<res3 << '\n';
-    std::cout << "tan " <<res4 << '\n';
-    std::cout << "ln " <<res5 << '\n';
-    std::cout << "log " <<res6 << '\n';
-    std::cout << "power "<<res7 << '\n';
-    std::cout << "pi "<<res8 << '\n';
-    std::cout << "sqrt "<<res9 << '\n';
-    std::cout << "root "<<res10 << '\n';
-    std::cout << "asin "<<res11 << '\n';
+   //cpp_dec_float_50 res = div_ta(100);
+   cpp_dec_float_50 res1 = exp_t(-1);
+   //cpp_dec_float_50 res2 = sin_t(25);
+   //cpp_dec_float_50 res3 = cos_t(-40);
+   //cpp_dec_float_50 res4 = tan_t(20);
+   //cpp_dec_float_50 res5 = ln_t(98);
+   //cpp_dec_float_50 res6 = log_t(75,3);
+   //cpp_dec_float_50 res7 = power_t(-5, 7);
+   //cpp_dec_float_50 res8 = pi();
+   cpp_dec_float_50 res9 = sqrt_t(2);
+   //cpp_dec_float_50 res10 = root_t(6,3);
+   //cpp_dec_float_50 res11 = asin_t(0.23);
+
+    //std::cout << "div_t " << res << '\n';
+    //std::cout << "exp " << res1 << '\n';
+    //std::cout << "sin " <<res2 << '\n';
+    //std::cout << "cos " <<res3 << '\n';
+    //std::cout << "tan " <<res4 << '\n';
+    //std::cout << "ln " <<res5 << '\n';
+    //std::cout << "log " <<res6 << '\n';
+    //std::cout << "power "<<res7 << '\n';
+    //std::cout << "pi "<<res8 << '\n';
+    //std::cout << "sqrt "<<res9 << '\n';
+    //std::cout << "root "<<res10 << '\n';
+    //std::cout << "asin "<<res11 << '\n';
 
 
 
-   double r1 = sin_t(3./7) + ln_t(2); 
+   cpp_dec_float_50 r1 = sin_t(3./7) + ln_t(2); 
    std::cout << r1  << "\n";
 
 
 
-   double r11 = root_t(r1, 3);
+   cpp_dec_float_50 r11 = root_t(r1, 3);
    std::cout << r11  << "\n";
 
 
 
-   double r2 = cos_t(res9);
+   cpp_dec_float_50 r2 = cos_t(res9);
    std::cout << r2  << "\n";
 
 
 
-   double pi_ = pi();
+   cpp_dec_float_50 pi_ = pi();
    std::cout << pi_  << "\n";
 
 
 
-   double r3 = log_t(res1, pi_);
+   cpp_dec_float_50 r3 = log_t(res1, pi_);
    std::cout << r3  << "\n";
 
 
-   double final = (r11 / r2) + r3;
+   cpp_dec_float_50 final = (r11 / r2) + r3;
    std::cout << final << "\n";
  
 }
